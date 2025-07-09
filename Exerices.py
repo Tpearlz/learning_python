ce1b77eb18e30770d9937f67f7f1531e461eba9c
Tpearlz <omoboyowa.tolulope@gmail.com>
2025-07-08 22:27:18 -0700

    Assigning Values

---
+++ Exerices.py
@@ -0,0 +1,15 @@
+
+# ▶ 1. Creating Variables and Assigning Values
+name = "Tolu"
+age = 35
+height = 1.75  # in meters
+is_student = False
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is student:", is_student)
+
+
+
+
