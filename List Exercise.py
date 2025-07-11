b773168707cf755c807230ee8cf65dd60e857ddb
Tpearlz <omoboyowa.tolulope@gmail.com>
2025-07-10 22:03:33 -0700

    Update List Exercise.py
    Accessing List Elements

--- List Exercise.py
+++ List Exercise.py
@@ -6,6 +6,16 @@ print("Initial List;", fruits)
 fruits.append("orange")
 print("After adding 'orange':",fruits)
 
+#2. Accessing List Elements
+print("First fruit:", fruits[0])
+# index starts at 0
+print("Last fruit:", fruits[-1])
+# negative index starts from end
+
+# Print the second and third fruits using slicing
+print("Second and third fruits:", fruits[1:3])
+
+
 
 
 
