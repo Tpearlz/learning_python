c1dd5bf30283d67e587ff6da284e4a2d79da2223
Tpearlz <omoboyowa.tolulope@gmail.com>
2025-07-10 23:47:16 -0700

    Create List of Lists Exercise.py
    Creating List of Lists

---
+++ List of Lists Exercise.py
@@ -0,0 +1,8 @@
+#1. Creating a List of Lists (2D List)
+# Let's say we have student scores for 3 students in 3 subjects
+scores = [
+    [85, 92, 78],   # Student 1
+    [76, 88, 90],   # Student 2
+    [90, 91, 95]    # Student 3
+]
+print("Initial scores:", scores)
