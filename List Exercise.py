d0b1c3d48781f8c26b0e8c00fb794a3ccd2b61bb
Tpearlz <omoboyowa.tolulope@gmail.com>
2025-07-10 22:02:25 -0700

    Create List Exercise.py
    Listing

---
+++ List Exercise.py
@@ -0,0 +1,14 @@
+#1 Create a List
+fruits = ["apple","banana","cherry","mango"]
+print("Initial List;", fruits)
+
+#Add "orange" to the list and print the updated list
+fruits.append("orange")
+print("After adding 'orange':",fruits)
+
+
+
+
+
+
+
