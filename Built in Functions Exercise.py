f73e6016ac12b185027f340af31974e8057458cc
Tpearlz <omoboyowa.tolulope@gmail.com>
2025-07-11 00:07:26 -0700

    Update Built in Functions Exercise.py
    Sum function

--- Built in Functions Exercise.py
+++ Built in Functions Exercise.py
@@ -6,5 +6,11 @@ print("Length of the list:", len(fruits))
 word = "Analytics"
 print("Length of 'Analytics':", len(word))
 
+#2. sum() - Sum of all elements in a list
+numbers = [10, 20, 30, 40]
+total = sum(numbers)
+print("Sum of numbers:", total)
 
+# Use sum() to find the total of [5, 15, 25, 35]
+print("Sum of [5, 15, 25, 35]:", sum([5, 15, 25, 35]))
 
