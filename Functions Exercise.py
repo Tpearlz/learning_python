98bbf7a027a2b9733252f82bd5b1a567ff4aa96b
Tpearlz <omoboyowa.tolulope@gmail.com>
2025-07-14 12:28:54 -0700

    Update Functions Exercise.py
    Call_greet_user

--- Functions Exercise.py
+++ Functions Exercise.py
@@ -9,3 +9,14 @@ greet()
 # Create a function named `welcome_user` that prints "Welcome to ACA!"
 def welcome_user():
     print("Welcome to ACA!")
+#2. Function with Parameters
+
+def greet_user(name):
+    print(f"Hello, {name}! Great to see you.")
+
+# Call the function with an argument
+greet_user("Tolu")
+
+# Call `greet_user()` with your own name
+greet_user("Tolu")
+
