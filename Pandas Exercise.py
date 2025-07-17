e40a45f58de1e1292e90735633b7007634ca0a20
Tpearlz <omoboyowa.tolulope@gmail.com>
2025-07-17 05:43:19 -0700

    Update Pandas Exercise.py
    veiwing_and_selecting_data

--- Pandas Exercise.py
+++ Pandas Exercise.py
@@ -22,3 +22,18 @@ print("\nDataFrame:\n", df)
 
 # Writing CSV
 # df.to_csv('output.csv', index=False)  # Uncomment to save file
+
+#3. Basic Operations
+# Viewing Data
+print("\nHead:\n", df.head())      # First 5 rows
+print("\nTail:\n", df.tail())      # Last 5 rows
+print("\nInfo:")
+df.info()                          # Info about columns
+print("\nDescribe:\n", df.describe())  # Summary stats
+
+
+# Selecting Columns and Rows
+print("\nName column:\n", df['Name'])
+print("\nName & Age columns:\n", df[['Name', 'Age']])
+print("\nRow by index (iloc):\n", df.iloc[0])
+print("\nRow by label (loc):\n", df.loc[0])
