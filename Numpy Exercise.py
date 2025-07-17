72604f3d1aea917052c3bf64a4288525b2d77af9
Tpearlz <omoboyowa.tolulope@gmail.com>
2025-07-17 05:19:00 -0700

    Update Numpy Exercise.py
    array_properties

--- Numpy Exercise.py
+++ Numpy Exercise.py
@@ -9,3 +9,10 @@ print("1D Array:\n",a)
 #create a 2D array (matrix)
 b = np.array([[1,2], [3,4]])
 print("2D Array:\n",b)
+
+#2 Array Properties
+print("Shape:", a.shape)
+print("Dimensions:", a.ndim)
+print("Data type:", a.dtype)
+print("Total elements:", a.size)
+
